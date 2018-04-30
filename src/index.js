42c438398a85c48d46307b965b26989c3680c11a
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-30 17:50:31 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// Code goes here
+// In a file called `Tweet.js`
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Tweet extends React.Component {
+  render() {
+    return (
+      <div className="tweet">
+        <img src="http://twitter.com/some-avatar.png" className="tweet__avatar" />
+        <div className="tweet__body">
+          <p>We are writing this tweet in JSX. Holy moly!</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tweet;
+
+// In a file in the same directory
+
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('root')
+);
